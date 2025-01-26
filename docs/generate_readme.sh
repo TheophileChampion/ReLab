4ef67f1e6145365e7e1cf9775f98c9ad88ad6d8b
Theophile Champion <txc314@student.bham.ac.uk>
2025-01-26 13:42:32 +0000

    Update Readme

--- docs/generate_readme.sh
+++ docs/generate_readme.sh
@@ -6,7 +6,5 @@ echo "## 🚀 ReLab" > ./docs/README.md
 cat ./README.md | tr "\n" "\r" | sed -e "s/.*-------\r\(.*\)<!-- toc -->.*/\1/" | tr "\r" "\n" >> ./docs/README.md
 
 cat ./README.md | sed -n -e '/## Installation/,$p' \
-                 | sed -e "s/\[In-Depth Tutorial\](Tutorial)/in-depth tutorial/" \
                  | sed -e "s/\[installed\](\#installation)/installed/" \
                  | tr "\n" "\r" | sed -r -e "s/## Documentation([^#]|\r)*//" | tr "\r" "\n" >> ./docs/README.md
-
