1a53dfc009de80dc7b34de167227ac42259bb0e3
Theophile Champion <txc314@student.bham.ac.uk>
2025-02-02 15:23:27 +0000

    Remove linter errors

--- tests/inc/agents/memory/test_data_buffer.hpp
+++ tests/inc/agents/memory/test_data_buffer.hpp
@@ -18,48 +18,48 @@ using namespace relab::agents::memory;
  * A class storing the parameters of the data buffer tests.
  */
 class DataBufferParameters {
-  public:
-    int capacity;
-    int n_steps;
-    float gamma;
-    float initial_priority;
-    int n_children;
+ public:
+  int capacity;
+  int n_steps;
+  float gamma;
+  float initial_priority;
+  int n_children;
 
-  public:
-    /**
-     * Create a structure storing the parameters of the data buffer tests.
-     * @param capacity the number of experiences the buffer can store
-     * @param n_steps the number of steps for which rewards are accumulated in
-     * multistep Q-learning
-     * @param gamma the discount factor
-     * @param initial_priority the initial priority given to first elements
-     * @param n_children the number of children each node has
-     */
-    DataBufferParameters(
-        int capacity, int n_steps, float gamma, float initial_priority = 1,
-        int n_children = 10
-    );
+ public:
+  /**
+   * Create a structure storing the parameters of the data buffer tests.
+   * @param capacity the number of experiences the buffer can store
+   * @param n_steps the number of steps for which rewards are accumulated in
+   * multistep Q-learning
+   * @param gamma the discount factor
+   * @param initial_priority the initial priority given to first elements
+   * @param n_children the number of children each node has
+   */
+  DataBufferParameters(
+      int capacity, int n_steps, float gamma, float initial_priority = 1,
+      int n_children = 10
+  );
 
-    /**
-     * Create a structure storing the parameters of the data buffer tests.
-     */
-    DataBufferParameters();
+  /**
+   * Create a structure storing the parameters of the data buffer tests.
+   */
+  DataBufferParameters();
 };
 
 /**
  * A fixture class for testing the data buffer.
  */
 class TestDataBuffer : public testing::TestWithParam<DataBufferParameters> {
-  public:
-    DataBufferParameters params;
-    std::unique_ptr<DataBuffer> buffer;
-    std::vector<torch::Tensor> observations;
+ public:
+  DataBufferParameters params;
+  std::unique_ptr<DataBuffer> buffer;
+  std::vector<torch::Tensor> observations;
 
-  public:
-    /**
-     * Setup of th fixture class before calling a unit test.
-     */
-    void SetUp();
+ public:
+  /**
+   * Setup of th fixture class before calling a unit test.
+   */
+  void SetUp();
 };
 }  // namespace relab::test::agents::memory::impl
 
--- tests/inc/helpers/test_deque.hpp
+++ tests/inc/helpers/test_deque.hpp
@@ -19,28 +19,28 @@ using namespace relab::helpers;
  * A class storing the parameters of the deque tests.
  */
 class DequeParameters {
-  public:
-    std::vector<int> elements;
-    int max_size;
-    int n_pops;
-    int length;
+ public:
+  std::vector<int> elements;
+  int max_size;
+  int n_pops;
+  int length;
 
-  public:
-    /**
-     * Create a structure storing the parameters of the deque tests.
-     * @param elements the elements to push in the deque
-     * @param max_size the maximum number of elements the deque can contain
-     * @param n_pops the number of elements to pop from the queue
-     * @param length the expected length of the deque at the end of the test
-     */
-    DequeParameters(
-        const std::initializer_list<int> &elements, int max_size, int n_pops, int length
-    );
+ public:
+  /**
+   * Create a structure storing the parameters of the deque tests.
+   * @param elements the elements to push in the deque
+   * @param max_size the maximum number of elements the deque can contain
+   * @param n_pops the number of elements to pop from the queue
+   * @param length the expected length of the deque at the end of the test
+   */
+  DequeParameters(
+      const std::initializer_list<int> &elements, int max_size, int n_pops, int length
+  );
 
-    /**
-     * Create a structure storing the parameters of the deque tests.
-     */
-    DequeParameters();
+  /**
+   * Create a structure storing the parameters of the deque tests.
+   */
+  DequeParameters();
 };
 
 /**
@@ -64,33 +64,33 @@ enum class PopType {
  * A fixture class for testing the deque.
  */
 class TestDeque : public testing::TestWithParam<DequeParameters> {
-  public:
-    DequeParameters params;
-    std::unique_ptr<Deque<int>> queue;
+ public:
+  DequeParameters params;
+  std::unique_ptr<Deque<int>> queue;
 
-  public:
-    /**
-     * Compare the inpt queue to the expected result.
-     * @param queue the input queue
-     * @param result the expected result
-     */
-    static void compare(Deque<int> &queue, const std::deque<int> &result);
+ public:
+  /**
+   * Compare the inpt queue to the expected result.
+   * @param queue the input queue
+   * @param result the expected result
+   */
+  static void compare(Deque<int> &queue, const std::deque<int> &result);
 
-    /**
-     * Create the expected result of the test.
-     * @param params the test parameters
-     * @param push_type the type of push performed in the test
-     * @param pop_type the type of pop performed in the test
-     * @return the expected result of the test
-     */
-    static std::deque<int> getResult(
-        const DequeParameters &params, const PushType &push_type, const PopType &pop_type
-    );
+  /**
+   * Create the expected result of the test.
+   * @param params the test parameters
+   * @param push_type the type of push performed in the test
+   * @param pop_type the type of pop performed in the test
+   * @return the expected result of the test
+   */
+  static std::deque<int> getResult(
+      const DequeParameters &params, const PushType &push_type, const PopType &pop_type
+  );
 
-    /**
-     * Setup of th fixture class before calling a unit test.
-     */
-    void SetUp();
+  /**
+   * Setup of th fixture class before calling a unit test.
+   */
+  void SetUp();
 };
 }  // namespace relab::test::helpers::impl
 
